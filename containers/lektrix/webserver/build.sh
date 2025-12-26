8afe1259e10516b4d00ae810266e105f53b97dfe
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-12-26 11:16:48 +0100

    add lektrix webserver

---
+++ containers/lektrix/webserver/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IMAGE_TAG="lektrix/webserver:latest"
+
+echo "Building ${IMAGE_TAG}..."
+podman build \
+    --tag "${IMAGE_TAG}"  \
+    --file Containerfile \
+    .
+
+cp ./config/* /srv/containers/lektrix/config
+
+echo
+echo "You can run the container with: podman run -d -p 8080:80 ${IMAGE_TAG}"
