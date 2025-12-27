a768ecd6be75226dbeee2a18665bd1944d14304a
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-12-27 11:01:58 +0100

    add build-context

--- containers/lektrix/trend/build.sh
+++ containers/lektrix/trend/build.sh
@@ -8,9 +8,18 @@ IMAGE_TAG="lektrix/trend:latest"
 echo "Building ${IMAGE_TAG}..."
 podman build \
     --tag "${IMAGE_TAG}"  \
+    --build-context lektrix=/home/beheer/git/lektrix \
     --file Containerfile \
     .
 
 # cp -rv ./config/* /srv/containers/lektrix/config
 
 printf "\nYou can now enable/start the lektrix-trend.service to run this container.\n\n"
+
+
+# podman run -it --rm \
+#     -v /home/beheer/git/lektrix/bin:/app/scripts:ro \
+#     -v /srv/containers/lektrix/data:/app/data:rw \
+#     -v /srv/containers/lektrix/www:/app/www:rw \
+#     lektrix/trend:latest \
+#     bash
