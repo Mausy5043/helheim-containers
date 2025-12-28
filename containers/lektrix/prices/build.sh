5931dc6b9ad87b4010ef622b224baccbcd40d9d4
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-12-28 15:46:53 +0100

    add prices

---
+++ containers/lektrix/prices/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eu
+
+cd "$(dirname "$0")"
+
+IMAGE_TAG="lektrix/prices:latest"
+
+echo "Building ${IMAGE_TAG}..."
+podman build \
+    --tag "${IMAGE_TAG}"  \
+    --build-context lektrix=/home/beheer/git/lektrix \
+    --file Containerfile \
+    .
+
+# cp -rv ./config/* /srv/containers/lektrix/config
+
+printf "\nYou can now enable/start the lektrix-prices.service to run this container.\n\n"
+
+exit 0
+# shellcheck disable=SC2034
+podman run -it --rm  \
+    --name lektrix-prices-dev \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /home/beheer/git/lektrix/bin:/app/scripts:ro \
+    -v /srv/containers/lektrix/data:/app/data:rw \
+    lektrix/prices:latest \
+    bash
