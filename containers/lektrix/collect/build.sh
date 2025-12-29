fbce50372127c169c3d2049fe19ad6a156d1600c
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-12-29 15:32:39 +0100

    initial commit

---
+++ containers/lektrix/collect/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -eu
+
+cd "$(dirname "$0")"
+
+IMAGE_TAG="lektrix/collect:latest"
+
+echo "Building ${IMAGE_TAG}..."
+podman build \
+    --tag "${IMAGE_TAG}"  \
+    --build-context lektrix=/home/beheer/git/lektrix \
+    --file Containerfile \
+    .
+
+# cp -rv ./config/* /srv/containers/lektrix/config
+
+printf "\nYou can now enable/start the lektrix-collect.service to run this container.\n\n"
+
+exit 0
+# shellcheck disable=SC2034
+podman run -it --rm  \
+    --name lektrix-collect-dev \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /home/beheer/git/lektrix/bin:/app/scripts:rw \
+    -v /srv/containers/lektrix/data:/app/data:rw \
+    -v /srv/containers/lektrix/config:/app/config:rw \
+    lektrix/collect:latest \
+    bash
