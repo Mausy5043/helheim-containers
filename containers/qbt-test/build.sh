180fd5fbf7da52def6423a57fb3d2bb7fefd6ad3
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-01-02 16:38:43 +0100

    update

--- containers/qbt-test/build.sh
+++ containers/qbt-test/build.sh
@@ -20,13 +20,6 @@ exit 0
 # below is example command to copy&paste to run container interactively
 # shellcheck disable=SC2034
 
-    # --network=slirp4netns:allow_host_loopback=true \
-    # --dns=192.168.2.2 \
-    # --volume /home/beheer/git/lektrix/bin:/app/scripts:rw \
-    # --volume /srv/containers/lektrix/data:/app/data:rw \
-    # --volume /srv/containers/lektrix/config:/app/config:rw \
-    # --volume /srv/containers/lektrix/www:/app/www:rw \
-
 podman run -it --rm  \
     --name qbt-dev \
     --volume /etc/localtime:/etc/localtime:ro \
