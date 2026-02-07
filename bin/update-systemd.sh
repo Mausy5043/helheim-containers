8e4b80a3e707a5545e9486e2c34a1db5154fb457
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-02-07 21:14:07 +0100

    remove comment

--- bin/update-systemd.sh
+++ bin/update-systemd.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Consumes the worklist '/var/lib/sysupdate/containers-to-build.conf',
-# ... iterates through container names,
-# ... calls 'build-container.sh',
-# ... updates the worklist to remove a successfully built container.
-
 set -euo pipefail
 
 REPO_ROOT="${HOME}/git/helheim-containers"
