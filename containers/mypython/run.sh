f6176ea50091f437ceb9a8286e37c3328d297f3e
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-02-15 13:01:50 +0100

    python testing container

---
+++ containers/mypython/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eu
+
+IMAGE_TAG="mypython"
+CONTAINER_NAME="mypython"
+
+# Stop and remove any existing container
+podman rm -f "${CONTAINER_NAME}" 2>/dev/null || true
+
+echo "Starting ${CONTAINER_NAME}..."
+
+podman run -d --rm \
+  --name "${CONTAINER_NAME}" \
+  --volume /etc/localtime:/etc/localtime:ro \
+  "${IMAGE_TAG}"
+
+echo "${CONTAINER_NAME} is started"
---
+++ containers/mypython/stop.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu
+
+IMAGE_TAG="mypython"
+CONTAINER_NAME="mypython"
+
+echo "Stopping ${CONTAINER_NAME}..."
+podman stop "${CONTAINER_NAME}" 2>/dev/null || true
+# Stop and remove any existing test container
+podman rm -f "${CONTAINER_NAME}" 2>/dev/null || true
+
+echo "${CONTAINER_NAME} is stopped"
