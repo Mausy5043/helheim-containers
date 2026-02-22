cfcdbf4107aa109b8fd4c712522ff11b7525cd07
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-02-22 08:19:51 +0100

    Add GPU reset before boarding

--- containers/ollama/board.sh
+++ containers/ollama/board.sh
@@ -6,8 +6,10 @@ CONTAINER_NAME="ollama-test"
 PORT=11434
 
 echo "Boarding ${CONTAINER_NAME}..."
-# clear the cache first
-sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
+# reset the GPU
+rocm-smi --gpureset -d 0 >/dev/null || echo "GPU reset failed!"
+# reclaim buffered RAM
+sync; sync; sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
 podman exec -it "${CONTAINER_NAME}" /bin/bash 2>/dev/null || true
 
 echo "Ollama is deboarded"
