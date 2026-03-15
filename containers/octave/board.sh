e8fa5c1bd4d6c1f4e7bdcdb5bd12f34054e0619e
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-03-15 11:52:20 +0100

    share correct time

--- containers/octave/board.sh
+++ containers/octave/board.sh
@@ -7,6 +7,9 @@ IMAGE_TAG="${CONTAINER_NAME}:${TAG}"
 
 echo "Boarding ${CONTAINER_NAME}..."
 
-podman run --rm -it --name "${CONTAINER_NAME}" "${IMAGE_TAG}"
+podman run --rm -it \
+  --name "${CONTAINER_NAME}"
+  --volume /etc/localtime:/etc/localtime:ro \
+  "${IMAGE_TAG}"
 
 echo "${CONTAINER_NAME} is deboarded"
