a926109d6ccbdac4eaab18180e7428b10febfbda
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-05-14 11:05:20 +0200

    don;t publish 53 while testing

--- containers/pihole/run.sh
+++ containers/pihole/run.sh
@@ -13,12 +13,13 @@ echo "Starting ${CONTAINER_NAME}..."
 podman run -d --rm \
   --name "${CONTAINER_NAME}" \
   --cap-add=NET_ADMIN \
-  --publish "53:53" \
   --publish "28080:80" \
   --publish "28081:443" \
   --volume /etc/localtime:/etc/localtime:ro \
   --volume /srv/containers/pihole/etc:/etc/pihole:rw,U \
   --volume /srv/containers/pihole/dnsmasq:/etc/dnsmasq.d:rw,U \
   "${IMAGE_TAG}"
+# Add these options later:
+#  --publish "53:53" \
 
 echo "${CONTAINER_NAME} is starting on http://127.0.0.1"
