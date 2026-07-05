c5d6f87349ab2cf506946b11f26c82600af0e31a
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-07-05 10:23:16 +0200

    network=host does not work for publishing ports

--- containers/pihole/run.sh
+++ containers/pihole/run.sh
@@ -19,18 +19,11 @@ echo "Starting ${CONTAINER_NAME}..."
 #  --cap-add=NET_ADMIN \
 #  --cap-add=NET_RAW \
 #  --cap-add=CHOWN \
-#  --publish "53:53/tcp" \
-#  --publish "53:53/udp" \
-#  --publish "67:67/udp" \
-#  --publish "28080:80" \
-#  --publish "28081:443" \
 #  --volume /etc/localtime:/etc/localtime:ro \
 #  --volume /srv/containers/pihole/etc/hosts:/etc/hosts:ro \
 #  --volume /srv/containers/pihole/etc/pihole:/etc/pihole:rw,U \
 #  --volume /srv/containers/pihole/etc/dnsmasq:/etc/dnsmasq.d:rw,U \
 #  "${IMAGE_TAG}"
-# Add these options later?:
-#  --publish "123:123" \  # NTP
 systemctl --user start pihole_pod
 
 echo "${CONTAINER_NAME} is starting on http://127.0.0.1"
