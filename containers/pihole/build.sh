75b5c9a0ceb94fe1b2bcf684f2ffc39e61e0aa41
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-07-18 10:00:25 +0200

    support HEALTHCHECK

--- containers/pihole/build.sh
+++ containers/pihole/build.sh
@@ -7,6 +7,9 @@ CONTAINER_NAME="pihole"
 TAG="helheim"
 IMAGE_TAG="${CONTAINER_NAME}:${TAG}"
 
+# Support Docker-specific features like HEALTHCHECK:
+export BUILDAH_FORMAT=docker
+
 echo "Building ${IMAGE_TAG}..."
 podman build \
     --tag "${IMAGE_TAG}" \
